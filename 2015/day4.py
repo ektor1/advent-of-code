af6d16d1e416d3137b367e619f09b628ad15df64
ektor1 <hc.nlks@gmail.com>
2026-09-11 22:56:48 +0100

    Add day 4 solution

---
+++ 2015/day4.py
@@ -0,0 +1,21 @@
+import hashlib
+
+
+def get_md5_hash(input_str):
+    """TO DO: write your own md5 hash function"""
+    pass
+
+
+def find_secret_key(input, n_zeros) -> int:
+    num = 0
+    md5_hash = hashlib.md5(input.encode())
+    cur_hex_start = md5_hash.hexdigest()[:n_zeros]
+    leading_zeros = n_zeros * "0"
+
+    while cur_hex_start != leading_zeros:
+        num += 1
+        cur_input = input + str(num)
+        md5_hash = hashlib.md5(cur_input.encode())
+        cur_hex_start = md5_hash.hexdigest()[:n_zeros]
+
+    return num
